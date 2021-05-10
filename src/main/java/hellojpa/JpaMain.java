cf3c6591e593d3119ebb35358e816df3a15030a9
ohunyk <ohunyk@gmail.com>
2021-05-10 23:17:32 +0900

    lazy write

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,20 +14,15 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
-        try{
-            //not persist
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
+        try {
 
-            //persist (not database save)
-            System.out.println("===== BEFORE =====");
-            em.persist(member);
-            System.out.println("===== AFTER =====");
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-            Member findMember = em.find(Member.class, 101L);
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("====================");
 
             tx.commit();
         } catch (Exception e) {
